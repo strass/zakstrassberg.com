8198d2c8094eb28c0b896db60ed597523663e6e6
Zak Strassberg <zakstrassberg@gmail.com>
2016-07-27 20:04:36 -0700

    more work

---
+++ public/javascripts/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    console.log("ready!");
+    var maxH3Height;
+    resizeWorkH3();
+    $(window).resize(function() {
+        resizeWorkH3();
+    });
+
+    function resizeWorkH3() {
+        console.log("resized");
+        var maxH3Height = 0;
+        $.each($("#work article h3 a"),function() {
+            if ($(this).height() > maxH3Height) {
+                maxH3Height = $(this).height();
+                console.log(maxH3Height);
+            }
+            $.each($("#work article h3 a").height(maxH3Height))
+        });
+    }
+});
