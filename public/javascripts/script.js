7bda87a4f57eccc3df19f1c01635e4c4f6678e85
Zak Strassberg <zakstrassberg@gmail.com>
2016-07-28 10:49:00 -0700

    added testimonial carousel

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
     console.log("ready!");
-    var maxH3Height;
-    resizeWorkH3();
-    $(window).resize(function() {
-        resizeWorkH3();
-    });
 
-    function resizeWorkH3() {
-        console.log("resized");
-        var maxH3Height = 0;
-        $.each($("#work article h3 a"),function() {
-            if ($(this).height() > maxH3Height) {
-                maxH3Height = $(this).height();
-                console.log(maxH3Height);
-            }
-            $.each($("#work article h3 a").height(maxH3Height))
-        });
-    }
+    // var maxH3Height;
+    // resizeWorkH3();
+    // $(window).resize(function() {
+    //     resizeWorkH3();
+    // });
+
+    // function resizeWorkH3() {
+    //     console.log("resized");
+    //     var maxH3Height = 0;
+    //     $.each($("#work article h3 a"),function() {
+    //         if ($(this).height() > maxH3Height) {
+    //             maxH3Height = $(this).height();
+    //             console.log(maxH3Height);
+    //         }
+    //         $.each($("#work article h3 a").height(maxH3Height))
+    //     });
+    // }
+
+    $('#testimonialSlider').slick({
+        autoplay: true,
+        autoplaySpeed: 20000,
+        dots: true,
+        pauseOnHover: true,
+        speed: 1500,
+    });
 });
